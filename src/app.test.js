c71531cae15e5b3ecac64f156bee6238f8059072
Elijah <kcerhajile@gmail.com>
2023-11-08 13:00:25 -0500

    Set up tests for soon to be made createShip factory function

--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,18 @@
 import { createShip } from "./app"
 
 describe('Ship objects',()=>{
-    
+    const mockShip = createShip(4);
+    testShip.hit()
+    test('hit() increases # of hits by 1', ()=>{
+        expect(testShip.getHits()).toBe(1);
+    })
+    testShip.hit()
+    testShip.hit()
+    test('isSunk() should be false',()=>{
+        expect(testShip.isSunk()).toBe(false);
+    })
+    testShip.hit()
+    test('isSunk() should be true',()=>{
+        expect(testShip.isSunk()).toBe(true);
+    })
 })
